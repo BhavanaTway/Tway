dfe27b209282a8ebe64d9f31fdde114a3b1e767e
unknown <bhavana@twayitservices.com>
2024-03-05 17:04:41 +0530

    Add new feature

---
+++ src/app2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App22() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the Home Page!</p>
+    </div>
+  );
+}
+
+export default App22;
+
+
+
+
+
+
+
+
+
+
+
+
